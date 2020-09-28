75417091d3e39c972066e257964369bf1abe1f6e
etotmeni <elena.totmenina@intel.com>
2020-09-28 14:04:31 -0500

    Add sigs in old dispatcher condition

--- numba/core/decorators.py
+++ numba/core/decorators.py
@@ -226,7 +226,7 @@ def _jit(sigs, locals, target, cache, targetoptions, **dispatcher_args):
         return disp
 
     def __wrapper(func):
-        if target == 'npyufunc' or targetoptions.get('no_cpython_wrapper') == True:
+        if target == 'npyufunc' or targetoptions.get('no_cpython_wrapper') == True or sigs:
             disp = registry.dispatcher_registry[target]
             return wrapper(func, disp)
         from numba.dppl.target_dispatcher import TargetDispatcher
